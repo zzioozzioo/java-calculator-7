55c86326165ca95bcd79afc6a39cf3200f2150bb
zzioozzioo <zzioo1006@naver.com>
2024-11-16 17:55:31 +0900

    test: Separator 도메인에 대한 테스트 완료

--- src/test/java/calculator/domain/SeparatorTest.java
+++ src/test/java/calculator/domain/SeparatorTest.java
@@ -3,46 +3,86 @@ package calculator.domain;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class SeparatorTest {
-    @Test
-    void 입력_문자열에서_구분자를_찾을_수_있다() {
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:2, 1:2", "//;\\n3;4, 3;4"})
+    void 입력_문자열에서_숫자_추출_기능_테스트(String input, String actualResult) {
         //given
-        String input = "1:2";
         Separator separator = new Separator();
 
         //when
-        String result = separator.findSeparator(input);
+        String expectedResult = separator.findSeparator(input);
 
         //then
-        Assertions.assertThat(result).isEqualTo(",|:");
+        Assertions.assertThat(expectedResult).isEqualTo(actualResult);
     }
 
     @Test
-    void 입력_문자열을_커스텀_구분자를_기준으로_분리한다() {
+    void 값을_입력하지_않은_경우_결과는_0이다() {
         //given
-        String input = "//;\\n3;4";
+        String input = "";
         Separator separator = new Separator();
 
         //when
-        String result = separator.findSeparator(input);
+        separator.findSeparator(input);
+        List<Integer> actualNumbers = separator.separateNumber(input);
+        List<Integer> expectedNumbers = List.of(0);
 
         //then
-        Assertions.assertThat(result).isEqualTo(",|:|;");
+        Assertions.assertThat(actualNumbers).isEqualTo(expectedNumbers);
+    }
+
+//    @Test
+//    void 커스텀_구분자의_구분자만_입력하면_결과는_0이다() {
+//        //given
+//        String input = "//;\n";
+//        Separator separator = new Separator();
+//
+//        //when
+//        separator.findSeparator(input);
+//        List<Integer> actualNumbers = separator.separateNumber(input);
+//        List<Integer> expectedNumbers = List.of(0);
+//
+//        //then
+//        Assertions.assertThat(actualNumbers).isEqualTo(expectedNumbers);
+//    }
+
+    @Test
+    void 음수를_입력하면_예외가_발생한다() {
+        //given
+        String input = "-1,2,3";
+        Separator separator = new Separator();
+
+        separator.findSeparator(input);
+
+        //when & then
+        Assertions.assertThatThrownBy(() ->
+                separator.separateNumber(input)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 문자열을_구분자로_나눠_숫자를_추출한다() {
-        //given
-        String input = "//;\\n3;4";
+    void 커스텀_구분자의_구분자가_정확하지_않으면_예외가_발생한다() {
+        String input = "//;3;4";
         Separator separator = new Separator();
 
-        //when
-        String string = separator.findSeparator(input);
-        List<Integer> expectedNumbers = List.of(3, 4);
-        List<Integer> actualNumbers = separator.separateNumber(input, string);
+        separator.findSeparator(input);
 
-        //then
-        Assertions.assertThat(expectedNumbers).isEqualTo(actualNumbers);
+        //when & then
+        Assertions.assertThatThrownBy(() ->
+                separator.separateNumber(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 커스텀_구분자를_여러개_입력하면_예외가_발생한다() {
+        String input = "//;^%\\n4;3^5";
+        Separator separator = new Separator();
+
+        //when & then
+        Assertions.assertThatThrownBy(() ->
+                separator.findSeparator(input)).isInstanceOf(IllegalArgumentException.class);
     }
 }
