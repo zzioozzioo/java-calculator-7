bf9f55bc2bac92e9efd18d9fe4bc7706362aecf1
zzioozzioo <zzioo1006@naver.com>
2024-11-16 17:23:50 +0900

    feat: 전체적인 흐름 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import calculator.config.Config;
+import calculator.config.IoConfig;
+import calculator.controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        Controller controller = new Controller(
+                new IoConfig(new Config())
+        );
+        controller.run();
     }
 }
---
+++ src/main/java/calculator/controller/Controller.java
@@ -0,0 +1,49 @@
+package calculator.controller;
+
+import calculator.config.IoConfig;
+import calculator.domain.Calculator;
+import calculator.domain.Separator;
+import calculator.io.InputView;
+import calculator.io.OutputView;
+import java.util.List;
+
+public class Controller {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public Controller(IoConfig ioConfig) {
+
+        this.inputView = ioConfig.getInputView();
+        this.outputView = ioConfig.getOutputView();
+    }
+
+    public void run() {
+        turnOnCalculator();
+    }
+
+    private void turnOnCalculator() {
+        while (true) {
+            try {
+                Separator separator = new Separator();
+                String input = inputView.readInput();
+                List<Integer> numbers = getNumbers(separator, input);
+                long sum = getSum(numbers);
+                outputView.printSumResult(sum);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private List<Integer> getNumbers(Separator separator, String input) {
+        input = separator.findSeparator(input);
+        return separator.separateNumber(input);
+    }
+
+    private long getSum(List<Integer> numbers) {
+        Calculator calculator = new Calculator();
+        return calculator.calculate(numbers);
+    }
+}
