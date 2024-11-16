4c300b7800694a04671b650fdbdb2dbd4fc6181b
zzioozzioo <zzioo1006@naver.com>
2024-11-16 17:23:29 +0900

    refactor: 예외 처리 기능 수정
    - 입력이 빈 경우는 0으로 처리해야 하기 때문에 예외 처리에서 제외

--- src/main/java/calculator/io/InputValidator.java
+++ src/main/java/calculator/io/InputValidator.java
@@ -3,7 +3,7 @@ package calculator.io;
 public class InputValidator {
 
     public void validateEmptyInput(String input) {
-        if (input == null || input.isEmpty()) {
+        if (input == null) {
             throw new IllegalArgumentException("덧셈할 문자열을 입력해야 합니다. 다시 입력해 주세요.\n");
         }
     }
