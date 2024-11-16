215cb353fd745f7aa4698a9bb76f853c88bd1bc2
zzioozzioo <zzioo1006@naver.com>
2024-11-16 17:19:30 +0900

    feat: 예외 처리 기능 구현

--- src/main/java/calculator/domain/Separator.java
+++ src/main/java/calculator/domain/Separator.java
@@ -1,7 +1,6 @@
 package calculator.domain;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,42 +9,68 @@ public class Separator {
 
     public static final Pattern PATTERN = Pattern.compile("//(.*?)\\\\n");
     public static final String DEFAULT_SEPARATOR = ",|:";
+    public static String TOTAL_SEPARATOR;
 
+    void setTotalSeparator(String totalSeparator) {
+        TOTAL_SEPARATOR = totalSeparator;
+    }
 
     public String findSeparator(String input) {
         String separator = "";
         Matcher matcher = PATTERN.matcher(input); // 정규식과 일치하면 true
         if (matcher.find()) {
             separator = matcher.group(1); // 커스텀 구분자에 해당하는 문자 반환
+            separator = validateCustomSeparator(separator);
+            input = removeWithoutCalculationTarget(input);
         }
+        setTotalSeparator(DEFAULT_SEPARATOR + separator);
+        return input;
+    }
+
+    private String validateCustomSeparator(String separator) {
         if (separator.length() > 1) {
-            throw new IllegalArgumentException("커스텀 구분자는 하나의 문자여야 합니다.");
+            throw new IllegalArgumentException("커스텀 구분자는 하나의 문자여야 합니다. 다시 입력해 주세요.");
         }
         if (separator.length() == 1) {
             separator = "|" + separator;
         }
-        return DEFAULT_SEPARATOR + separator;
+        if (separator.length() == 0) {
+            throw new IllegalArgumentException("커스텀 구분자를 입력하지 않았습니다. 다시 입력해 주세요.");
+        }
+        return separator;
     }
 
-    public List<Integer> separateNumber(String input, String separator) {
-        input = removeWithoutCalculationTarget(input, separator);
+    public List<Integer> separateNumber(String input) {
         try {
-            return getNumbers(input, separator);
+            return getNumbers(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자만 계산할 수 있습니다.");
+            throw new IllegalArgumentException("숫자만 계산할 수 있습니다. 다시 입력해 주세요.");
         }
     }
 
-    private String removeWithoutCalculationTarget(String input, String separator) {
+    private String removeWithoutCalculationTarget(String input) {
+        // TODO: 인덱스로 자르면 안되고, \n 전까지 잘라야 함
         return input.substring(5);
     }
 
-    private static List<Integer> getNumbers(String input, String separator) {
+    private static List<Integer> getNumbers(String input) {
+        // NFE 여기로 옮기기!
         List<Integer> intNumbers = new ArrayList<>();
-        List<String> strNumbers = Arrays.asList(input.split(separator));
-        for (String number : strNumbers) {
-            intNumbers.add(Integer.parseInt(number));
+        String[] splitNumbers = input.split(TOTAL_SEPARATOR);
+        for (String number : splitNumbers) {
+            addValidNumberToList(number, intNumbers);
         }
         return intNumbers;
+    }
+
+    private static void addValidNumberToList(String number, List<Integer> intNumbers) {
+        if (number.trim().isEmpty()) {
+            number = "0";
+        }
+        int parsedInt = Integer.parseInt(number);
+        if (parsedInt < 0) {
+            throw new IllegalArgumentException("음수는 계산할 수 없습니다. 자연수만 입력해 주세요.");
+        }
+        intNumbers.add(parsedInt);
     }
 }
